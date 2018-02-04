0bdd571ded968786312248646321bdf850b57923
Ashwin Abraham <ashwinabraham96@gmail.com>
2018-02-04 14:19:30 +0000

    ANIMATIONS

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ jQuery(document).ready(function( $ ) {
     } else {
       $('.back-to-top').fadeOut('slow');
     }
+
+    if ($(this).scrollTop() >= $('#n').scrollTop()) {
+      $('#q').addClass('active');
+    }
   });
   $('.back-to-top').click(function(){
     $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
