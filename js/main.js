89af674996d4c5e52a9a2202978f0210fbaa03ac
Ashwin Abraham <ashwinabraham96@gmail.com>
2018-02-09 19:59:08 +0000

    item nums

--- js/main.js
+++ js/main.js
@@ -159,7 +159,7 @@ jQuery(document).ready(function( $ ) {
     autoplay: true,
     dots: true,
     loop: true,
-    responsive: { 0: { items: 2 }, 768: { items: 4 }
+    responsive: { 0: { items: 2 }, 768: { items: 2 }
     }
   });
 
